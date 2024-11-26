a5d32699e626f75e21801e18f1a8659be5b188c5
DawnKr <2168608696@qq.com>
2024-11-26 12:29:48 +0800

    fixed bug

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import os
 
+T = 7
+
 def max_min_normalize(file_location: str, output_location: str) -> None:
     for file_name in os.listdir(file_location):
         file_path = os.path.join(file_location, file_name)
@@ -8,8 +10,13 @@ def max_min_normalize(file_location: str, output_location: str) -> None:
         for key in df.columns.to_list()[3:7]:
             df[key] = df[key].astype(float)
             min_value = df[key].min()
-            down_value = df[key].max() - min_value
-            df[key] = df[key].map(lambda x: x - min_value / down_value)
+            max_value = df[key].max()
+            if key == 'pH*':
+                df[key] = df[key].map(lambda x: 1 - abs(x - T) / max(abs(x - min_value), abs(x - max_value)))
+            elif key == 'DO':
+                df[key] = df[key].map(lambda x: (x - min_value) / (max_value - min_value))
+            else:
+                df[key] = df[key].map(lambda x: (max_value - x) / (max_value - min_value))
         output_path = os.path.join(output_location, file_name)
         df.to_csv(output_path)
 
